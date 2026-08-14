8769166f8d4b568b90bf81b70c4a023bbece567a
meenakshee09 <meenaksheeadapelly09@gmail.com>
2026-08-14 10:34:40 +0530

    Add backend code

---
+++ backend/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class LoanApplication(BaseModel):
+    no_of_dependents: int
+    education: str
+    self_employed: str
+    income_annum: float
+    loan_amount: float
+    loan_term: int
+    cibil_score: int
+    residential_assets_value: float
+    commercial_assets_value: float
+    luxury_assets_value: float
+    bank_asset_value: float
